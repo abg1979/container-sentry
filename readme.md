7492b4ec6bfd90055e159727bce5b693af424602
Abhishek Gupta <oss@carbonbulls.com>
2024-08-28 12:22:55 +0530

    Adding url regex assignments.

--- readme.md
+++ readme.md
@@ -1,10 +1,17 @@
-# container-sentry
+# Container Sentry
 
-container-sentry is a [Firefox](https://www.mozilla.org/firefox/) extension to open any URL (not just those with a specific domain) in a multi-account container.
+Container Sentry is a [Firefox](https://www.mozilla.org/firefox/) extension to open any URL (not just those with a specific domain) in a multi-account container. It also allows
+to specify regular expressions for urls which are allowed to be loaded outside a container.
 
-The extension consists of a [settings page](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Implement_a_settings_page) (`src/settings/`) built with [Vue.js](https://vuejs.org/), and a [background script]() (`src/background/`). The settings page allows users to define regex patterns that, when matched against a URL loaded in the browser, will open that URL in a specified container tab. The background script listens for update events in browser tabs and performs the regex matching and opening of container tabs.
+This was needed because the VPN provider used by my employer opens a local html page which redirects to the authentication page.
+The [Always in Container](https://addons.mozilla.org/en-US/firefox/addon/always-in-container) intercepts this request and breaks the context which in the end fails the logon to VPN.
 
-Firefox's [Multi-Account Containers](https://support.mozilla.org/kb/containers) extension provides similar functionality out of the box, but only allows for matching URLs based on a domain. This extension allows for regex matching against the whole URL as opposed to exact matches based on domain.
+The extension consists of a [settings page](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Implement_a_settings_page) (`src/settings/`) built with [Vue.js](https://vuejs.org/), and a [background script]() (`src/background/`). 
+The settings page allows users to define regex patterns that, when matched against a URL loaded in the browser, will open that URL in a specified 
+container tab. The background script listens for update events in browser tabs and performs the regex matching and opening of container tabs.
+
+Firefox's [Multi-Account Containers](https://support.mozilla.org/kb/containers) extension provides similar functionality out of the box, but only allows for matching URLs based on 
+a domain. This extension allows for regex matching against the whole URL as opposed to exact matches based on domain.
 
 ## Installation
 
@@ -14,13 +21,14 @@ This extension can be installed at [addons.mozilla.org](https://addons.mozilla.o
 
 ### Prerequisites
 
-Development on, or building of, this extension requires Firefox, [Node.js](https://nodejs.org) v16+, and [Yarn](https://yarnpkg.com/getting-started) or [npm](https://docs.npmjs.com/). Older versions of Node will probably work, they just haven't been tested.
+Development on, or building of, this extension requires Firefox, [Node.js](https://nodejs.org) v20+, and [Yarn](https://yarnpkg.com/getting-started).
+Older versions of Node will probably work, they just haven't been tested.
 
 ### Development
 
 To load the extension in a development instance of Firefox with automatic reloading enabled, run:
 
-```
+```shell
 yarn install
 yarn start
 ```
@@ -29,9 +37,17 @@ yarn start
 
 To build and package the extension for distribution, run:
 
-```
+```shell
 yarn install
 yarn build
 ```
 
-This will run a webpack build an place output in `build/webpack/`, followed by packaging the extension using `web-ext` and placing output in `web-ext-artifacts/`.
+This will run a webpack build and place the output in `build/webpack/`, followed by packaging the extension using `web-ext` and placing output 
+in `web-ext-artifacts/`.
+
+### Credits
+
+This extenstion borrows a lot from the following extensions
+
+1. <https://addons.mozilla.org/en-US/firefox/addon/always-in-container> | <https://github.com/tiansh/always-in-container>
+2. <https://addons.mozilla.org/en-GB/firefox/addon/open-urls-in-container/> | <https://gitlab.com/hughblackall/open-urls-in-container>
--- src/background/background.js
+++ src/background/background.js
@@ -1,140 +1,200 @@
-; (async function () {
+;(async function () {
 
-  /*
-   * Ignore any pages which were assigned in Multi-Account Containers (MAC)
-   */
-  const MAC_ADDON_ID = '@testpilot-containers';
+    /*
+     * Ignore any pages which were assigned in Multi-Account Containers (MAC)
+     */
+    const MAC_ADDON_ID = '@testpilot-containers';
 
-  let macAddonEnabled = await (async function () {
-    try {
-      const macAddonInfo = await browser.management.get(MAC_ADDON_ID);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }());
-
-  const onMACAddonEnabledChange = enabled => info => {
-    if (info.id !== MAC_ADDON_ID) return;
-    macAddonEnabled = enabled;
-  };
-  browser.management.onInstalled.addListener(onMACAddonEnabledChange(true));
-  browser.management.onEnabled.addListener(onMACAddonEnabledChange(true));
-  browser.management.onUninstalled.addListener(onMACAddonEnabledChange(false));
-  browser.management.onDisabled.addListener(onMACAddonEnabledChange(false));
-
-  const isMACAssigned = async function (url) {
-    if (!macAddonEnabled) return false;
-
-    try {
-      const assignment = await browser.runtime.sendMessage(MAC_ADDON_ID, {
-        method: 'getAssignment',
-        url,
-      });
-      return Boolean(assignment);
-    } catch (e) {
-      return false;
-    }
-  };
-
-  const hasURLException = async function (url) {
-    // return true if multi account container is disabled
-    if (!macAddonEnabled) return true;
-
-    try {
-      console.debug("Fetching URL exceptions: ", url);
-      const { urlContainerMappings, urlExceptions } = await browser.storage.sync.get({ urlContainerMappings: [], urlExceptions: [] })
-      console.debug("Loaded exceptions : ", urlExceptions);
-      const urlsMatched = urlExceptions.reduce((matches, current) => {
-        if (url.match(current.pattern)) {
-          matches.push(current)
+    let macAddonEnabled = await (async function () {
+        try {
+            const macAddonInfo = await browser.management.get(MAC_ADDON_ID);
+            return true;
+        } catch (e) {
+            return false;
         }
-        return matches
-      }, [])
-      console.debug("Following urls matched...", url, urlsMatched);
-      return urlsMatched.length > 0;
-    } catch (e) {
-      return false;
-    }
-  };
+    }());
 
-  /*
-   * Check such pages and ask user to chose a container
-   */
-  /** @type {Map<number, { requestIds: Set<string>, urls: Set<string> }>} */
-  const canceledRequests = new Map();
+    const onMACAddonEnabledChange = enabled => info => {
+        if (info.id !== MAC_ADDON_ID) return;
+        macAddonEnabled = enabled;
+    };
+    browser.management.onInstalled.addListener(onMACAddonEnabledChange(true));
+    browser.management.onEnabled.addListener(onMACAddonEnabledChange(true));
+    browser.management.onUninstalled.addListener(onMACAddonEnabledChange(false));
+    browser.management.onDisabled.addListener(onMACAddonEnabledChange(false));
 
-  const cleanCancledRequest = tabId => {
-    if (canceledRequests.has(tabId)) {
-      canceledRequests.delete(tabId);
-    }
-  };
-  browser.webRequest.onCompleted.addListener(options => {
-    cleanCancledRequest(options.tabId);
-  }, { urls: ['<all_urls>'], types: ['main_frame'] });
+    const isMACAssigned = async function (url) {
+        if (!macAddonEnabled) return false;
 
-  browser.webRequest.onErrorOccurred.addListener(options => {
-    cleanCancledRequest(options.tabId);
-  }, { urls: ['<all_urls>'], types: ['main_frame'] });
+        try {
+            const assignment = await browser.runtime.sendMessage(MAC_ADDON_ID, {
+                method: 'getAssignment',
+                url,
+            });
+            return Boolean(assignment);
+        } catch (e) {
+            return false;
+        }
+    };
 
-  const shouldCancelEarly = function (tab, request) {
-    const tabId = tab.id;
-    const { requestId, url } = request;
-    if (!canceledRequests.has(tabId)) {
-      canceledRequests.set(tabId, {
-        requestIds: new Set([requestId]),
-        urls: new Set([url]),
-      });
-      setTimeout(() => { canceledRequests.delete(tabId); }, 2000);
-      return false;
-    }
-    const tabInfo = canceledRequests.get(tabId);
-    const shouldCancel = tabInfo.requestIds.has(requestId) || tabInfo.urls.has(url);
-    tabInfo.requestIds.add(requestId);
-    tabInfo.urls.add(url);
-    return shouldCancel;
-  };
+    const hasURLException = async function (url) {
+        // return true if multi account container is disabled
+        if (!macAddonEnabled) return true;
 
-  browser.webRequest.onBeforeRequest.addListener(async function containTab(request) {
-    console.debug("Received request for: ", request.url);
-    const tab = await browser.tabs.get(request.tabId);
+        try {
+            console.debug("Fetching URL exceptions: ", url);
+            const {urlContainerMappings, urlExceptions} = await browser.storage.sync.get({urlContainerMappings: [], urlExceptions: []})
+            console.debug("Loaded exceptions : ", urlExceptions);
+            const urlsMatched = urlExceptions.reduce((matches, current) => {
+                if (url.match(current.pattern)) {
+                    matches.push(current)
+                }
+                return matches
+            }, [])
+            console.debug("Following urls matched...", url, urlsMatched);
+            return urlsMatched.length > 0;
+        } catch (e) {
+            return false;
+        }
+    };
 
-    if (request.tabId === -1) return void 0;
-    if (tab.incognito) return void 0;
+    const doURLContainerMatchSwitch = async function (url) {
+        // return true if multi account container is disabled
+        if (!macAddonEnabled) return false;
 
-    try {
-      console.debug("Checking already contained: ", request.url);
-      await browser.contextualIdentities.get(tab.cookieStoreId);
-      console.debug("Already contained... Returning: ", request.url);
-      return void 0;
-    } catch (e) {
-      /* we are not contained yet */
-    }
+        try {
+            console.debug("Fetching URL container mappings", url);
+            const {urlContainerMappings, urlExceptions} = await browser.storage.sync.get({urlContainerMappings: [], urlExceptions: []})
+            console.debug("Loaded url container mappings", urlContainerMappings);
+            const urlsMatched = urlContainerMappings.reduce((matches, current) => {
+                if (url.match(current.pattern)) {
+                    matches.push(current)
+                }
+                return matches
+            }, [])
+            console.debug("Following urls matched...", url, urlsMatched);
+            // if there are multiple matches, we will use the first one
+            let containerName = urlsMatched.length > 0 ? urlsMatched[0].containerName : null;
+            if (!containerName) {
+                console.debug("No container assigned for URL: ", url);
+                return false;
+            }
+            console.debug("URL has a container assigned... Trying to switch to it: ", request.url, containerName);
+            const container = await browser.contextualIdentities.query({
+                name: containerName,
+            });
+            if (container.length === 0) {
+                console.debug("Container not found... Skipping switch: ", request.url, containerName);
+                return false;
+            }
+            const cookieStoreId = container[0].cookieStoreId;
+            if (cookieStoreId && typeof cookieStoreId === 'string') {
+                console.debug(`Replacing tab. cookieStoreId was '${cookieStoreId}'.`);
+                browser.tabs.create({
+                    url: request.url + '',
+                    cookieStoreId,
+                });
+                browser.tabs.remove(tab.id);
+                return true;
+            }
+            console.debug(`Not replacing tab. cookieStoreId was '${cookieStoreId}'.`);
+            return false;
+        } catch (e) {
+            return false;
+        }
+    };
 
-    console.debug("Checking if MAC is enabled: ", request.url);
-    if (await isMACAssigned(request.url)) return void 0;
-    console.debug("MAC is enabled... Continuing: ", request.url);
-    console.debug("Checking if URL has exception: ", request.url);
-    if (await hasURLException(request.url)) return void 0;
-    console.debug("URL does not have exception... Continuing: ", request.url);
+    /*
+     * Check such pages and ask user to choose a container
+     */
+    /** @type {Map<number, { requestIds: Set<string>, urls: Set<string> }>} */
+    const canceledRequests = new Map();
 
-    if (request && shouldCancelEarly(tab, request)) {
-      return { cancel: true };
-    }
+    const cleanCancelledRequest = tabId => {
+        if (canceledRequests.has(tabId)) {
+            canceledRequests.delete(tabId);
+        }
+    };
+    browser.webRequest.onCompleted.addListener(options => {
+        cleanCancelledRequest(options.tabId);
+    }, {urls: ['<all_urls>'], types: ['main_frame']});
 
-    console.debug("Building container chooser UI: ", request.url);
-    const choseUrl = new URL(browser.runtime.getURL('/togo/index.html'));
-    choseUrl.searchParams.set('go', request.url);
-    await browser.tabs.create({
-      url: choseUrl + '',
-      active: tab.active,
-      index: tab.index,
-      windowId: tab.windowId,
-    });
-    browser.tabs.remove(tab.id);
+    browser.webRequest.onErrorOccurred.addListener(options => {
+        cleanCancelledRequest(options.tabId);
+    }, {urls: ['<all_urls>'], types: ['main_frame']});
 
-    return { cancel: true };
+    const shouldCancelEarly = function (tab, request) {
+        const tabId = tab.id;
+        const {requestId, url} = request;
+        if (!canceledRequests.has(tabId)) {
+            canceledRequests.set(tabId, {
+                requestIds: new Set([requestId]),
+                urls: new Set([url]),
+            });
+            setTimeout(() => {
+                canceledRequests.delete(tabId);
+            }, 2000);
+            return false;
+        }
+        const tabInfo = canceledRequests.get(tabId);
+        const shouldCancel = tabInfo.requestIds.has(requestId) || tabInfo.urls.has(url);
+        tabInfo.requestIds.add(requestId);
+        tabInfo.urls.add(url);
+        return shouldCancel;
+    };
 
-  }, { urls: ['<all_urls>'], types: ['main_frame'] }, ['blocking']);
+    browser.webRequest.onBeforeRequest.addListener(async function containTab(request) {
+        console.debug("Received request for: ", request.url);
+        const tab = await browser.tabs.get(request.tabId);
+
+        if (request.tabId === -1) return void 0;
+        if (tab.incognito) return void 0;
+
+        try {
+            console.debug("Checking already contained: ", request.url);
+            await browser.contextualIdentities.get(tab.cookieStoreId);
+            console.debug("Already contained... Returning: ", request.url);
+            return void 0;
+        } catch (e) {
+            /* we are not contained yet */
+        }
+
+        console.debug("Checking if MAC is enabled: ", request.url);
+        if (await isMACAssigned(request.url)) {
+            console.debug("MAC is disabled... Not doing anything: ", request.url);
+            return void 0;
+        }
+        console.debug("MAC is enabled... Continuing: ", request.url);
+        console.debug("Checking if URL has exception: ", request.url);
+        if (await hasURLException(request.url)) {
+            console.debug("URL has exception... Not doing anything: ", request.url);
+            return void 0;
+        }
+        console.debug("URL does not have exception... Continuing: ", request.url);
+
+        if (request && shouldCancelEarly(tab, request)) {
+            return {cancel: true};
+        }
+
+        // check if url has a container assigned
+        console.debug("Checking if URL has container assigned: ", request.url);
+        if (await doURLContainerMatchSwitch(request.url)) {
+            return {cancel: true};
+        }
+
+        console.debug("Building container chooser UI: ", request.url);
+        const choseUrl = new URL(browser.runtime.getURL('/togo/index.html'));
+        choseUrl.searchParams.set('go', request.url);
+        await browser.tabs.create({
+            url: choseUrl + '',
+            active: tab.active,
+            index: tab.index,
+            windowId: tab.windowId,
+        });
+        browser.tabs.remove(tab.id);
+
+        return {cancel: true};
+
+    }, {urls: ['<all_urls>'], types: ['main_frame']}, ['blocking']);
 
 }());
